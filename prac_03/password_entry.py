51b0ddf99bfd87b0b09da8d580665e5e41344976
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-08-11 13:38:32 +1000

    Refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,17 +1,26 @@
 """Check user input password for correct length"""
 
+MINIMUM_PASSWORD_LENGTH = 6
+
 
 def main():
 
-    minimum_password_length = 6
-    password = input("Enter password: ")
+    password = get_password()
 
-    while len(password) < minimum_password_length:  # Check password against length requirements.
-        print("Password is too small. Must be {} characters long.".format(minimum_password_length))
-        password = input("Enter password: ")
+    print_asterisks(password)
 
-    for char in password:   # Print "*" for every character in password.
+
+def print_asterisks(password):
+    for char in password:  # Print "*" for every character in password.
         print("*", end='')
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < MINIMUM_PASSWORD_LENGTH:  # Check password against length requirements.
+        print("Password is too small. Must be {} characters long.".format(MINIMUM_PASSWORD_LENGTH))
+        password = input("Enter password: ")
+    return password
 
 
 main()
