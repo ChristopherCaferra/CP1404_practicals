07dbbc97680b637bded56211e75557e568158481
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-08-11 14:37:12 +1000

    Add function to calculate test result.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,14 +1,25 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    grade = calculate_grade(score)
+    print("Final grade is: {}".format(grade))
+
+
+def calculate_grade(score):
+    # calculate grade and return string to main
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
