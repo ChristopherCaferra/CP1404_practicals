6500c9b08461b2742ae841dc30242841edadcd80
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-08-15 18:50:51 +1000

    Add function for security checker program.

--- prac_04/bad_security_checker.py
+++ prac_04/bad_security_checker.py
@@ -3,9 +3,19 @@ Inadequate security checker program
 """
 
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
-            'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer','bob']
-username_input = input("Enter your username: ")
-while username_input not in usernames:
-    print("Access denied")
-print("Access granted")
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username_input = input("Enter your username: ")
+    print(check_username(username_input, usernames))
+
+
+def check_username(string, usernames):
+    while string not in usernames:
+        print("Access denied")
+        string = input("Enter your username: ")
+    return "Access granted"
+
+
+main()
