4740b9cd024f99958fcd7459afa8968b6d9cc309
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-08-30 20:06:32 +1000

    Add name method for Cars.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,12 +1,12 @@
 """CP1404/CP5632 Practical - Client code to use the Car class."""
 # Note that the import has a folder (module) in it.
 
-from prac_06.car import Car
+from cp1404practicals.prac_06.car import Car
 
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("ferrari", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,10 +15,13 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-    limo = Car(100)
+    limo = Car("limo", 100)
     limo.add_fuel(20)
     print("limo fuel = {}".format(limo.fuel))
     limo.drive(115)
     print("limo Odometer = {}".format(limo.odometer))
-    print(limo.__str__(limo))
+    print(limo)
+    print("{}".format(limo))
+
+
 main()
