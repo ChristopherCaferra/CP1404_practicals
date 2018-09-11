e12ec5e5e36df593aab793a53c45ecde6f4cc001
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-09-11 20:42:50 +1000

    Add error checking and remove convert button.

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -9,20 +9,35 @@ from kivy.core.window import Window
 
 
 class ConvertMilesToKilometresApp(App):
+    """ ConvertMilesToKilometresApp is a Kivy App for converting miles to kilometres. """
+    MILES_TO_KM = 1.60934
 
     def build(self):
+        """ Build the Kivy app from the kv file. """
         Window.size = (400, 200)
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_m_to_km.kv')
         return self.root
 
-    def handle_conversion_calculation(self, value):
-        result = value * 1.609
+    def handle_calculate_conversion(self):
+        """ Handle conversion calculation and output to label widget. """
+        value = self.get_valid_miles()
+        result = value * self.MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
-    def handle_increment(self, value, increment):
-        result = value + increment
-        self.root.ids.input_number.text = str(result)
+    def handle_increment(self, increment):
+        """ increments miles by 1/-1 when up/down buttons pressed. Updates label with new conversion result. """
+        value = self.get_valid_miles() + increment
+        self.root.ids.input_number.text = str(value)
+        self.handle_calculate_conversion()
+
+    def get_valid_miles(self):
+        """ Error handling for invalid input that is not numeric. returns '0.0' if input is not numeric. """
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0.0
 
 
 ConvertMilesToKilometresApp().run()
