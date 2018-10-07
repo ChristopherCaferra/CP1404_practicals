515ee6324d669f84c9689c52489ccbf630fe2c23
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-10-07 17:23:57 +1000

    Change program to check for folders

--- prac_09/sort_files.py
+++ prac_09/sort_files.py
@@ -8,17 +8,32 @@ import shutil
 
 
 def main():
+    extensions = []
+    extension = ''
     os.chdir('FilesToSort')
     for filename in os.listdir('.'):
         file_type = filename[filename.find('.') + 1:]
-        if os.path.isdir(filename):
-            continue
-        else:
-            try:
-                os.mkdir(file_type)
-            except FileExistsError:
-                pass
-        shutil.move(filename, file_type + '/' + filename)
+        extensions.append(file_type)
+        for extension in extensions:
+            if not os.path.isdir(extension):
+                os.mkdir(extension)
+        try:
+            shutil.move(filename, extension + '/' + filename)
+        except shutil.Error:
+            pass
+#
+# def main():
+#     os.chdir('FilesToSort')
+#     for filename in os.listdir('.'):
+#         file_type = filename[filename.find('.') + 1:]
+#         if os.path.isdir(filename):
+#             continue
+#         else:
+#             try:
+#                 os.mkdir(file_type)
+#             except FileExistsError:
+#                 pass
+#         shutil.move(filename, file_type + '/' + filename)
 
 
 main()
