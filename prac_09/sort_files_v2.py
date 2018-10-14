1a10ccffc73dbf408048e8da61cbfd6c866d4bcb
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-10-14 20:08:55 +1000

    Change dictionary and remove list

--- prac_09/sort_files_v2.py
+++ prac_09/sort_files_v2.py
@@ -11,23 +11,22 @@ import shutil
 def main():
     """Program to sort file extensions based on categories chosen by the user"""
     extensions = {}
-    files = []
     os.chdir('FilesToSort')
     for filename in os.listdir('.'):        # save file names to list and extensions to dictionary
         file_type = filename[filename.find('.') + 1:]
         if filename.find(file_type):
-            files.append(filename)
-            extensions.update({file_type: ""})
-    for extension in extensions.keys():     # get file category for every extension
+            extensions[file_type] = ""
+    for extension in extensions:     # get file category for every extension
         category = input("What category would you like to sort {} files into? ".format(extension))
-        extensions.update({extension: category})
+        extensions[extension] = category
     for category in extensions.values():    # make file category folder if it does not exist already
         if not os.path.isdir(category):
             os.mkdir(category)
             print(category)
-    for filename in files:                  # move files into category folders
+    for filename in os.listdir('.'):                  # move files into category folders
         file_type = filename[filename.find('.') + 1:]
-        shutil.move(filename, extensions.get(file_type) + "/" + filename)
+        if filename.find(file_type):
+            shutil.move(filename, extensions.get(file_type) + "/" + filename)
 
 
 main()
