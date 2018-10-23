d50cdee28c3e5fe048823e98bfaf5d80c8159cc1
ChristoperCaferra <christopher.caferra@my.jcu.edu.au>
2018-10-24 01:37:53 +1000

    Complete TODO's

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return ((s + " ") * n).strip()
 
 
 def is_long_word(word, length=5):
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -32,8 +32,6 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
@@ -41,23 +39,24 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
-
+    assert test_car.fuel == 10
+    test_car = Car()
+    assert test_car.fuel == 0
 
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+# (PyCharm may see your >>> doctest comments and run doctests anyway.)
+doctest.testmod()
+
+
 # (don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
+
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
@@ -66,3 +65,21 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+
+def make_sentence(s):
+    """
+    >>> make_sentence("hello")
+    'Hello.'
+    >>> make_sentence("It is an ex parrot.")
+    'It is an ex parrot.'
+    >>> make_sentence("here I am.")
+    'Here I am.'
+    """
+    phrase = []
+    for char in s:
+        phrase.append(char)
+    phrase[0] = phrase[0].upper()
+    if phrase[-1] != ".":
+        phrase.append(".")
+    return "".join(phrase)
+
